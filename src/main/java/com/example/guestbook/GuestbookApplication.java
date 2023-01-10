2b7b88540ab78b426b33a2e05a6eb3ca68397856
qkrthdud002 <qkrthdud002@gmail.com>
2023-01-11 01:16:46 +0900

    @EnableJpaAuditing 설정 추가

--- src/main/java/com/example/guestbook/GuestbookApplication.java
+++ src/main/java/com/example/guestbook/GuestbookApplication.java
@@ -2,8 +2,10 @@ package com.example.guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA를 이용하면서 AuditingEntityListener를 활성시키기 우해서는 프로젝트에 추가해야 한다.
 public class GuestbookApplication {
 
     public static void main(String[] args) {
